fffb1728af062a0e3101d0ca7206a0427a1981bf
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-12 20:33:19 +0530

    Test `get_linked_css_url`

--- html-parser/main.py
+++ html-parser/main.py
@@ -1,5 +1,6 @@
 from utils.get_linked_scripts_url import get_linked_scripts_url
+from utils.get_linked_css import get_linked_css_url
 
-html = get_linked_scripts_url("https://hashable.space")
+html = get_linked_css_url("https://hashable.space")
 
 print(html)
