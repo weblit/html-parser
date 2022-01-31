1a1a3509a6e7eb43453e8f7a237adb2a0fec6067
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-31 18:10:35 +0530

    add css source finder

--- html-parser/main.py
+++ html-parser/main.py
@@ -1,11 +1,15 @@
 from src.HtmlParser import HtmlParser
 from src.find_source_script import find_source_script
+from src.find_source_css import find_source_css
 
 htmlparser = HtmlParser("./html-parser/tests/index.html")
 
-print(htmlparser.find_linked_css_count())
+stylesheet = htmlparser.find_linked_css_urls()
 
 scripts = htmlparser.find_linked_scripts_url()
 
 for script in scripts:
     print(find_source_script(script))
+
+for css in stylesheet:
+    print(find_source_css(css))
