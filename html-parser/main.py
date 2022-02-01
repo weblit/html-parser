925a5105407e4fcc55c4ac62524f29b69dcefd08
Haneen Mahdin <haneenmahdin@gmail.com>
2022-02-01 07:26:08 +0530

    fix relative path for scripts

--- html-parser/main.py
+++ html-parser/main.py
@@ -4,15 +4,16 @@ from src.find_source_script import find_source_script
 from src.find_source_css import find_source_css
 from src.relative_path import relative_path
 
-htmlparser = HtmlParser("https://apple.com")
+htmlparser = HtmlParser("http://localhost:3000/")
 
 stylesheet = htmlparser.find_linked_css_urls()
 
 scripts = htmlparser.find_linked_scripts_url()
 
 for script in scripts:
-    print(find_source_script(script))
+    source = find_source_script(script)
+    print("Relative path", relative_path(source, "http://localhost:3000"))
 
 for css in stylesheet:
     source = find_source_css(css)
-    print("Relative path", relative_path(source, "apple.com"))
+    print("Relative path", relative_path(source, "http://localhost:3000"))
