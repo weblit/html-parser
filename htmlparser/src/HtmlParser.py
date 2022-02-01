dab2c4cd792537b8ffd71c506a863e43cc2a0ffe
Haneen Mahdin <haneenmahdin@gmail.com>
2022-02-01 09:35:51 +0530

    add utilitles inside the class `HtmlParser`

--- htmlparser/src/HtmlParser.py
+++ htmlparser/src/HtmlParser.py
@@ -1,5 +1,9 @@
+from htmlparser.helpers.clean_attributes import clean_attributes
 from utils.get_linked_css_url import get_linked_css_url
 from utils.get_linked_scripts_url import get_linked_scripts_url
+from src.find_source_css import find_source_css
+from src.find_source_script import find_source_script
+from src.relative_path import relative_path
 
 class HtmlParser:
     def __init__(self, url) -> None:
@@ -17,5 +21,14 @@ class HtmlParser:
     def find_linked_scripts_count(self) -> int:
         return len(get_linked_scripts_url(self.url))
 
+    def get_css_paths(self) -> list(str):
+        css = self.find_linked_css_urls()
+        source = find_source_css(css)
 
+        return relative_path(clean_attributes(source), self.url)
 
+    def get_js_paths(self) -> list(str):
+        css = self.find_linked_scripts_url()
+        source = find_source_script(css)
+
+        return relative_path(clean_attributes(source), self.url)
